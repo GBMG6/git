71d1f7eafdb8a117fa940bae5164fd04c8191e83
GBMG6 <ars123dltjdwns@gmail.com>
2025-03-21 01:30:27 +0900

    c:\Users\USER\Desktop\inchTocm.py

--- test_python.py1.py
+++ test_python.py1.py
@@ -1,1 +1,8 @@
-print("hello world")
+input_a = float(input("cm -> inch 변환>>"))
+print(input_a, "인치는", input_a * 2.54)
+
+input_a = float(input("킬로그램 -> 파운드 변환>>"))
+print(input_a, "킬로그램은", input_a * 2.2, "파운드입니다.")
+
+input_a = float(input("반지름 -> 원의 둘레와 넓이이 변환>>"))
+print("반지름이", input_a, "인 원의 둘레는 ", (string)input_a * 6.28,"이고 원의 넓이는 ", input_a * input_a * 3.14,"입니다.")
