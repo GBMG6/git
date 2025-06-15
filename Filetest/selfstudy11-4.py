32e65be10c649d1585c95e4c55cdaad8985bf1e7
GBMG6 <ars123dltjdwns@gmail.com>
2025-06-15 20:43:20 +0900

    Filetest/selfstudy11-4.py

--- Filetest/selfstudy11-4.py
+++ Filetest/selfstudy11-4.py
@@ -1,8 +1,10 @@
 inFp, outFp = None, None
 inStr = ""
+i=input("소스 파일명을 입력하세요 :")
+l=input("타킷 파일명을 입력하세요 :")
 
-inFp = open
-OutFp = open("C:\\Users\\USER\\Desktop\\파이썬\\Filetest\\data3.txt", "w", encoding="utf-8")
+inFp = open(i)
+OutFp = open(l, "w", encoding="utf-8")
 
 inList = inFp.readlines()
 for inStr in inList : 
@@ -10,4 +12,4 @@ for inStr in inList :
 
 inFp.close()
 outFp.close()
-print("------파일이 정상적으로 복사되었음------")
+print("------",i," 파일이 ",l, "파일로 정상적으로 복사되었음------")
